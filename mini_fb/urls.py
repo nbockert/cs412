f164762bec3dabc22551508d943a41f74181a8eb
Nora Bockert <nbockert@bu.edu>
2024-11-04 15:11:05 -0500

    fixed logic error

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,6 +5,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'), # generic class-based view
@@ -17,6 +18,6 @@ urlpatterns = [
     path('profile/<int:pk>/friend_suggestions/', views.ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
     path(r'profile/<int:pk>/add_friend/<int:other_pk>', views.CreateFriendView.as_view(), name='add_friend'),
     path('profile/<int:pk>/news_feed', views.ShowNewsFeedView.as_view(), name='news_feed'),
-    path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name='login'), ## NEW
-    path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name='logout'), ## NEW
+    path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html',), name='logout'), ## NEW
 ]
